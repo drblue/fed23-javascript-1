bc7e1652d746527cd75626b1b07d5957a5dae97d
Johan Nordström <johan@digitalvillage.se>
2023-10-23 14:31:19 +0200

    String concatenation, variables and constants.

--- 01-basics/script.js
+++ 01-basics/script.js
@@ -1,3 +1,59 @@
 // Detta visar en alert-ruta på webbsidan
-alert("Hej från script.js!");
-alert("I AM HANGRY!!!!!!!!!!!!!!!!");
+// alert("Hej från script.js!");
+// alert("I AM HANGRY!!!!!!!!!!!!!!!!");
+
+// Skriver ut ett meddelande till webbläsarens konsol
+// console.log("Hej från script.js!");
+// console.log("Hej igen från script.js!");
+
+/*
+let msg;
+msg = "Hej från msg!";
+
+console.log(msg);
+console.log(msg);
+
+msg = "Hej igen från msg!";
+
+console.log(msg);
+*/
+
+// let first_name = "Johan";
+
+/*
+// Naming Conventions
+
+// Camel Case
+camelCase
+
+// Snake Case
+snake_case
+
+// Upper Camel Case
+UpperCamelCase
+PascalCase
+
+// Kebab Case / BBQ Case
+kebab-case
+bbq-case
+*/
+
+let firstName = "Johan";
+console.log(firstName);
+
+firstName = "Pelle";
+console.log(firstName);
+
+const middleName = "Bengt";
+
+const lastName = "Nordström";
+console.log(lastName);
+
+console.log(firstName + middleName + lastName);   // "PelleBengtNordström"
+console.log("Pelle" + "Nordström");
+
+console.log(firstName + " " + lastName);   // "Pelle Nordström"
+console.log("Pelle" + " " + "Nordström");   // "Pelle Nordström"
+
+let fullName = firstName + " " + lastName;
+console.log("Your name is: " + fullName);
---
+++ datatypes.md
@@ -0,0 +1,13 @@
+# Datatypes in JavaScript
+
+## "Primitive Data Types"
+
+| English   | Svenska     | Exempel                        |
+| --------- | ----------- | ------------------------------ |
+| String    | Sträng      | "Hello world" 'Hello world' "" |
+| Number    | Tal         | 1, 2, 3, 3.14, 42, 1337        |
+| BigInt    | Tal         | Gigantiska tal                 |
+| Boolean   | Boolsk      | true, false                    |
+| Null      | Null        | Avsaknad av ett värde          |
+| Undefined | Odefinierad | Har inte en datatyp ännu       |
+| Symbol    | Symbol      | (har med objekt att göra)      |
