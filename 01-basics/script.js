012279c2c209b05ed35f7ea7ab78a5d83c63e2c1
Johan Nordström <johan@digitalvillage.se>
2023-10-23 15:11:23 +0200

    Fun with strings!

--- 01-basics/script.js
+++ 01-basics/script.js
@@ -38,11 +38,12 @@ kebab-case
 bbq-case
 */
 
+/*
 let firstName = "Johan";
 console.log(firstName);
 
 firstName = "Pelle";
-console.log(firstName);
+// console.log(firstName);
 
 const middleName = "Bengt";
 
@@ -57,3 +58,46 @@ console.log("Pelle" + " " + "Nordström");   // "Pelle Nordström"
 
 let fullName = firstName + " " + lastName;
 console.log("Your name is: " + fullName);
+
+// String Properties
+console.log("The length of your full name is: " + fullName.length);
+
+// String Methods
+console.log("Your full name (but screaming): " + fullName.toUpperCase());
+console.log("Your full name (but whispered): " + fullName.toLowerCase());
+console.log("The first char of your name is: " + fullName[0]);
+
+console.log("The first 'e' exists at index: " + fullName.indexOf("e"));
+
+let newName = fullName.replace("Pelle", "Alicia");    // case sensitive
+console.log("New name: " + newName);
+console.log("Your (old) name is: " + fullName);
+
+let palle = fullName.replace("e", "a");  // replaces only first occurance
+console.log("Palle's name is: " + palle);
+
+// "Pelle"
+//  01234
+let partOfPelle = firstName.slice(1, 3); // "el"
+console.log("Part of Pelle: " + partOfPelle);
+
+let alsoPartOfPelle = firstName.substring(1, 3); // "el"
+console.log("Also part of Pelle: " + alsoPartOfPelle);
+
+let morePartsOfPelle = firstName.substring(1); // "elle"
+console.log("Also part of Pelle: " + morePartsOfPelle);
+*/
+
+let email;
+// email = "jn@thehiveresistance.com"; // ska returnera 20 + 'com'
+// email = "some@dude.org"; // ska returnera 9 + 'org'
+// email = "bgates@microsoft.com"; // ska returnera 16 + 'com'
+// email = "some.brittish.dude@bt.co.uk"; // ska returnera 24 + 'uk'
+
+// Hitta sista punkten i variabeln `email` och skriv ut till console
+// Ska fungera oavsett vad för värde `email` har, utan att behöva ändra koden
+// "The last dot for email EMAIL is at index: 42"
+
+// Med hjälp av föregående kod, ta ut allting EFTER sista punkten i
+// `email` och skriv till console
+// "The top domain for email EMAIL is: DOMAIN"
