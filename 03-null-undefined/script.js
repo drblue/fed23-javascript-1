ab8745db7da3697f95f4568c01e562aea9f04429
Johan Nordström <johan@digitalvillage.se>
2023-10-30 11:01:40 +0100

    Null vs undefined.

---
+++ 03-null-undefined/script.js
@@ -0,0 +1,22 @@
+/*
+let score; // undefined
+console.log("type of score:", typeof score); // "undefined"
+
+score = null;
+console.log("type of score after setting to null:", typeof score); // "null"
+
+score = 42;
+console.log("type of score after setting score to 42:", typeof score);
+
+score = "i win";
+console.log("type of score after set to text:", typeof score);
+*/
+
+let a; // undefined
+let b = null; // null
+
+console.log("a:", a);
+console.log("b:", b);
+
+console.log("a (undefined) + 5", a + 5);
+console.log("b (null) + 5", b + 5);      // 0 + 5
