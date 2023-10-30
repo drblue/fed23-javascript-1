e918256c4d1adde2719c3fb99681d22384be56bc
Johan Nordström <johan@digitalvillage.se>
2023-10-30 15:57:12 +0100

    Workshop - Password Checker boilerplate.

---
+++ 10-workshop-password-check/script.js
@@ -0,0 +1,32 @@
+/**
+ * Workshop: Password Checker
+ *
+ * Skriv kod som kollar att lösenordet i variabeln password har
+ * - minst 6 tecken varav minst två specialtecken enligt nedan
+ * - minst 8 tecken varav minst ett specialtecken enligt nedan
+ * - eller har minst 12 tecken och minst 1 bindestreck
+ * - eller har minst 16 tecken
+ *
+ * Googla för att hitta lämpliga metoder för att lösa uppgiften.
+ *
+ * Du ska alltså precis som i första dagens workshop ta bort kommentarerna
+ * en efter en och testa att alla olika kombinationer av lösenord fungerar
+ * och ger rätt resultat.
+ */
+
+let password;
+// password = "password"; // inte giltigt
+// password = "pa$sword"; // giltigt
+// password = "p@ssw%rd"; // giltigt
+// password = "pa$$word"; // giltigt
+// password = "secretpassword"; // inte giltigt
+// password = "secret-password"; // giltigt
+// password = "such-password-much-secure-very-long"; // giltigt
+
+const specialChars = [
+	"@", "$", "%", "*", "^", "<", ">", "?", "!", "(", ")", "[", "]", "{", "}", "'"
+];
+
+console.log(`🕵🏻 Checking password '${password}'`);
+
+// Skriv din kod här
