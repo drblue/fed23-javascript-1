fdc7a89a89eefdc8f2f04415b18fc37293b93358
Johan Nordström <johan@digitalvillage.se>
2023-11-01 14:33:16 +0100

    Switch statements.

--- 11-loops/script.js
+++ 11-loops/script.js
@@ -56,10 +56,52 @@ console.log("The sum of all numbers is:", sum);
  * do while loop
  * runs *at least* 1 time
  */
-let i = 5;
-do {
-	// do this
-	console.log("in do while-loop, i is:", i);
+// let i = 5;
+// do {
+// 	// do this
+// 	console.log("in do while-loop, i is:", i);
 
-	i++;  // increase i by 1
-} while (i < 5);
+// 	i++;  // increase i by 1
+// } while (i < 5);
+
+/**
+ * Switch
+ */
+
+/*
+let grade = "MVP";
+if (grade === "VG") {
+	console.log("🥳 Nice!! You got a VG");
+
+} else if (grade === "G") {
+	console.log("🆗 Excellent! You got a G");
+
+} else if (grade === "IG") {
+	console.log("😔 Sorry, you got an IG");
+
+} else {
+	console.log("😡 Why you try 🐅 cheating?!");
+
+}
+*/
+
+let grade = "G";
+switch (grade) {
+	case "VG":
+		console.log("🥳 Nice!! You got a VG");
+		break;
+
+	case "G":
+		console.log("🆗 Excellent! You got a G");
+		break;
+
+	case "IG":
+		console.log("😔 Sorry, you got an IG");
+		break;
+
+	default:
+		console.log("DEFAULT ALL ZE THINGS!!!!!!!!!!!!");
+		break;
+}
+
+console.log("I'm done with loops");
