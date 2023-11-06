e3edfb746542302b9e4d9850dd8d34aaa51e2358
Johan Nordström <johan@digitalvillage.se>
2023-11-06 12:00:15 +0100

    Return values in functions.

--- 14-functions/script.js
+++ 14-functions/script.js
@@ -29,8 +29,37 @@ const greetUser = function(username, time = "day") {
 	console.log(`Good ${time}, ${username}`);
 }
 
-console.log("I am before");
-greetUser("Johan", "morning");
-greetUser("Pelle");
-greetUser();
-console.log("I am after");
+// console.log("I am before");
+// greetUser("Johan", "morning");
+// greetUser("Pelle");
+// greetUser();
+// console.log("I am after");
+
+// let username = "Johan";
+// const sayHi = function(username) {
+// 	username = username.toUpperCase();
+// 	console.log(`Hi ${username}`);
+// }
+
+// sayHi(username);
+// console.log(username);
+
+const calcBoxCircumference = function(width, height) {
+	return width * 2 + height * 2;
+}
+
+// let sum1 = calcBoxCircumference(20, 40);    // let sum1 = 120
+// let sum2 = calcBoxCircumference(30, 60);
+// console.log("sum1", sum1);
+// console.log("sum2", sum2);
+
+const w = 20;
+const h = 40;
+const ci = calcBoxCircumference(w, h);   // calcBoxCircumference(20, 40)
+if (ci > 100) {
+	console.log("Thats a big box");
+} else {
+	console.log("smol box");
+}
+
+console.log("Circumference of box with height 20 and width 40 is:", calcBoxCircumference(40, 20));
