b1ae3f167b6809c6cf033bf8b573532f28e5fa58
Johan Nordström <johan@digitalvillage.se>
2023-11-08 15:44:36 +0100

    Get elements from DOM, get content from elements and change their content.

--- 20-dom/script.js
+++ 20-dom/script.js
@@ -3,3 +3,32 @@
  *
  */
 
+const firstLink = document.querySelector('#link-1');
+
+// Get the element's parent
+const firstLinkParent = firstLink.parentElement;
+
+// Get the parent's next sibling
+const firstLinkParentNextSibling = firstLinkParent.nextElementSibling;
+
+// Get the first paragraph on the page
+const firstParagraph = document.querySelector('p');
+
+// Log the first paragraphs HTML
+console.log(firstParagraph.innerHTML);
+
+// Log the first paragraphs text
+console.log(firstParagraph.innerText);
+
+// Get the first paragraph with 'error'-class on the page
+const firstErrorParagraph = document.querySelector('p.error');
+
+// Log the first error-paragraphs HTML
+console.log(firstErrorParagraph.innerHTML);
+
+// Log the first error-paragraphs text
+console.log(firstErrorParagraph.innerText);
+
+// We can also change the content of an element
+firstParagraph.innerText = "Look ma, my first paragraph!";
+firstParagraph.innerText += " Oh I forgot this!";
