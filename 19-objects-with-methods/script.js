a4be76b30e157ef94cbad72e6b543967869c6b1c
Johan Nordström <johan@digitalvillage.se>
2023-11-08 15:06:09 +0100

    Primitive vs Complex datatypes, and pass by value vs pass by reference.

--- 19-objects-with-methods/script.js
+++ 19-objects-with-methods/script.js
@@ -51,3 +51,70 @@ const meowJr = {
 // petSpeak(meowJr);
 
 // console.log("this outside of an object:", this);
+
+/**
+ * Primitive datatypes vs reference datatypes
+ */
+
+/*
+// Primitive datatypes are copied by **value**
+let name1 = "Johan";
+let name2 = name1;  // name2 = "Johan"
+
+name1 = "Pelle";
+
+// console.log(name1, name2);
+
+// complex data types (arrays + objects) are copied by **reference**
+const littleBarksby = barksby;
+
+littleBarksby.name = "Barksby Jr";
+
+// console.log(barksby.name);			// "Barksby Jr"
+// console.log(littleBarksby.name);		// "Barksby Jr"
+
+const names = ["Johan", "Pelle"];
+const friends = names;
+friends.push("Agda");
+console.log(names === friends);			// true
+*/
+
+/*
+// Primitive Data Types are passed by **value**
+const happyBirthday = (name, age) => {  // name = "Pelle", age = 2
+	age++;
+	console.log(`Happy Birthday, ${name}`, age);
+}
+
+let pelle = "Pelle";
+let pelle_age = 2;
+
+happyBirthday(pelle, pelle_age);  // Happy Birthday, Pelle 3
+
+console.log("Pelle's age outside of function:", pelle_age);  // 2 😨
+*/
+
+// Reference Data Types (like objects and arrays) are passed by **reference**
+const happyPetBirthday = (pet) => {
+	pet.age++;
+	console.log(`Happy Birthday, ${pet.name}`, pet.age);
+}
+
+console.log("Meow Jr age before birthday:", meowJr.age);  // 1
+happyPetBirthday(meowJr);  // "Happy Birthday, Meow Jr 2"
+console.log("Meow Jr age after birthday:", meowJr.age);
+
+/*
+const friends = [];
+
+const addFriend = (names, name) => {
+	names.push(name);
+}
+
+addFriend(friends, "Johan");
+addFriend(friends, "Pelle");
+addFriend(friends, "Agda");
+addFriend(friends, "Beatrice");
+*/
+
+const pets = [barksby, meowJr];
