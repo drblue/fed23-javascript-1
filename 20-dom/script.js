d83fb4d109e42eeedfbb762332181e0a7ac3bdc2
Johan Nordström <johan@digitalvillage.se>
2023-11-13 11:21:56 +0100

    DOM traversal, attributes, classList.

--- 20-dom/script.js
+++ 20-dom/script.js
@@ -3,6 +3,7 @@
  *
  */
 
+/*
 const firstLink = document.querySelector('#link-1');
 
 // Get the element's parent
@@ -32,3 +33,39 @@ console.log(firstErrorParagraph.innerText);
 // We can also change the content of an element
 firstParagraph.innerText = "Look ma, my first paragraph!";
 firstParagraph.innerText += " Oh I forgot this!";
+*/
+
+const alertEl = document.querySelector(".alert");
+console.log('alertEl:', alertEl);
+
+console.log('alertEl innerHTML:', alertEl.innerHTML);
+console.log('alertEl innerText:', alertEl.innerText);
+console.log('alertEl textContent:', alertEl.textContent);
+
+// Change content
+// alertEl.innerText = "Such error, much danger, very fail";
+// alertEl.innerText = "Such <strong>error</strong>, much danger, very fail";
+// alertEl.innerHTML = "Such <strong>error</strong>, much danger, very fail";
+
+// Say hi to the user
+/*
+const h2El = document.querySelector("h2#greetings");
+const username = prompt("What are thy name?");
+// h2El.innerHTML = `Hello ${username}`; // BIG NO-NO!
+h2El.innerText = `Hello ${username}`;
+*/
+
+/*
+const linkListEl = document.querySelector('ul');
+console.log('linkListEl:', linkListEl.children);  // HTMLCollection, does not have `.forEach()`-method
+
+// Convert HTMLCollection to Array
+const liEls = Array.from(linkListEl.children);  // now we can use `.forEach()`
+
+// OR, we can use querySelector and querySelectorAll on the linkListEl
+const liEls2 = linkListEl.querySelectorAll('li');
+*/
+
+// Get and set attributes
+// document.querySelector("img").getAttribute("src")
+// document.querySelector("img").setAttribute("src", "https://media.tenor.com/tNGtfhqJWP0AAAAd/haxor.gif")
