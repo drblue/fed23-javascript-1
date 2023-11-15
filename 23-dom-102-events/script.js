03ae6347b2aca39a8893a3583787617d275386c5
Johan Nordström <johan@digitalvillage.se>
2023-11-15 11:10:42 +0100

    DOM 102 boilerplate.

---
+++ 23-dom-102-events/script.js
@@ -0,0 +1,11 @@
+/**
+ * DOM 102 + events
+ *
+ * classList (used on an element)
+ * - add		adds a class (if it doesn't already exist)
+ * - remove		removes a class (if it doesn't already exist)
+ * - toggle		adds a class if it doesn't already exist, otherwise removes it
+ * - replace	replaces a class with another class
+ * - contains	returns true/false if class exists
+ */
+
