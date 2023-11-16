a9bfccbf15e5fd051705c6aa7c87782f70f6aa47
Johan Nordström <johan@digitalvillage.se>
2023-11-16 14:10:45 +0100

    Workshop: Simple Todos - Step 1 + 2.

--- 24-simple-todos/script.js
+++ 24-simple-todos/script.js
@@ -33,25 +33,61 @@ formCreateTodoEl.addEventListener("submit", (e) => {
 	e.preventDefault();
 
 	// Get title from input-field
+	const newTodoTitle = inputNewTodoTitleEl.value;
 
 	// Abort if input is empty
+	if (newTodoTitle === "") {
+		return;
+	}
 
 	// Create a new todo-object
+	const newTodo = {
+		title: newTodoTitle,
+		completed: false,
+	}
 
 	// Add it to the todos-array
+	todos.push(newTodo);
 
 	// Render a representation of the
 	// updated todos-array
+	renderTodos();
 
 	// Finally, clear the input-field
+	inputNewTodoTitleEl.value = "";
 });
 
 // Render a representation of the todos-array to the DOM
 const renderTodos = () => {
 	// Clear any existing listitems from the DOM
+	todolistEl.innerText = "";
 
 	// Loop over the todos-array and create a new
 	// listitem for each todoitem
+	todos.forEach((todo) => {
+		/*
+		const cssCompleted = todo.completed ? "completed" : "";
+
+		todolistEl.innerHTML += `
+			<li class="list-group-item ${cssCompleted}">
+				${todo.title}
+			</li>
+		`;
+		*/
+
+		// create a new li element
+		const todoEl = document.createElement("li");
+		todoEl.innerText = todo.title;
+		todoEl.classList.add("list-group-item");
+
+		if (todo.completed) {
+			todoEl.classList.add("completed");
+		}
+
+		// append the new li element to the ul element
+		todolistEl.append(todoEl);
+	});
 }
 
 // Render the initial representation of the todos-array
+renderTodos();
