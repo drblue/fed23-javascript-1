39da1710648967d3e8459191e533a80533ea3a95
Johan Nordström <johan@digitalvillage.se>
2023-11-16 16:16:40 +0100

    Clock goes tick, tock ⌚️.

--- 26-clock/script.js
+++ 26-clock/script.js
@@ -6,3 +6,22 @@
 // get reference to element with id `clock`
 const clockEl = document.querySelector('#clock');
 
+const tick = () => {
+	// get current date and time
+	const now = new Date();
+
+	// output current time to `#clock`-element
+	clockEl.innerText = now.toLocaleTimeString();
+
+	if (now.getDay() === 4 && now.getHours() >= 16) {
+		clockEl.innerText += " 🥳";
+	}
+}
+
+// Start interval timer
+// We could stop the time by storing the id from
+// setInterval and calling `clearInterval(clockId)`
+setInterval(tick, 1000);
+
+// Get curren time on page load
+tick();
