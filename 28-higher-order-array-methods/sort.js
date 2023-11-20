e6fd3a64706449003cacf2fa4ce67fe318313bea
Johan Nordström <johan@digitalvillage.se>
2023-11-20 13:29:53 +0100

    We can even sort objects based on a property!

--- 28-higher-order-array-methods/sort.js
+++ 28-higher-order-array-methods/sort.js
@@ -26,32 +26,85 @@ console.log("Names in reverse alphabetical order 🤩:", names);
 */
 
 // numbers
-const numbers = [ 47, 5, 21, 50, 28, 18, 47, 13, 24, 8, 18, 15, 11, 43, 7, 102, 1337, 33, 52 ];
-console.log("Numbers in chaos:", numbers);
+// const numbers = [ 47, 5, 21, 50, 28, 18, 47, 13, 24, 8, 18, 15, 11, 43, 7, 102, 1337, 33, 52 ];
+// console.log("Numbers in chaos:", numbers);
 
 // sort numbers (won't sort numerically!)
 // numbers.sort();
 // console.log("Numbers in proper? order:", numbers);
 
 // sort numbers NUMERICALLY
-numbers.sort( (a, b) => {
-	console.log("comparing a vs b", a, b);
+// numbers.sort( (a, b) => {
+// 	// shorter version of the logic below
+// 	return (a - b);
 
-	// if a is less than b (i.e. a should be sorted BEFORE b)
-	if (a < b) {
-		console.log("a is less than b");
+// 	/*
+// 	console.log("comparing a vs b", a, b);
+
+// 	// if a is less than b (i.e. a should be sorted BEFORE b)
+// 	if (a < b) {
+// 		console.log("a is less than b");
+// 		return -1337;
+// 	}
+
+// 	// if a is greater than b (i.e. a should be sorted AFTER b)
+// 	if (a > b) {
+// 		console.log("a is greater than b");
+// 		return 40000;
+// 	}
+
+// 	// a must be equal to b
+// 	console.log("a is equal to b");
+// 	return 0;
+// 	*/
+// } );
+
+// EVEN SHORTER NUMBER SORTING 🤯
+// numbers.sort( (a, b) => a - b );
+
+// console.log("Numbers in proper! order:", numbers);
+
+// We can even sort objects based on a property
+const students = [
+	{
+		name: "Johan",
+		points: -1337,
+	},
+	{
+		name: "Kim",
+		points: 3,
+	},
+	{
+		name: "Alicia",
+		points: 42,
+	},
+	{
+		name: "Elliot",
+		points: 88,
+	},
+	{
+		name: "Maja",
+		points: 35,
+	},
+];
+
+console.log("students before sort:", students);
+
+students.sort( (a, b) => {
+	// shorthand (only works if the attribute we compare is a **NUMBER**)
+	return (a.points - b.points);
+
+	/*
+	if (a.points < b.points) {
 		return -1;
 	}
 
-	// if a is greater than b (i.e. a should be sorted AFTER b)
-	if (a > b) {
-		console.log("a is greater than b");
+	if (a.points > b.points) {
 		return 1;
 	}
 
-	// a must be equal to b
-	console.log("a is equal to b");
 	return 0;
+	*/
 } );
 
-console.log("Numbers in proper! order:", numbers);
+console.log("students after sort:", students);
