1201727ede0925139c0a94f29649a45590035e3c
Johan Nordström <johan@digitalvillage.se>
2023-11-20 11:22:05 +0100

    Higher Order Array Methods - Skeleton.

---
+++ 28-higher-order-array-methods/sort.js
@@ -0,0 +1,16 @@
+/**
+ * Higher Order Array Methods
+ *
+ * .sort() <--
+ * .filter()
+ * .find()
+ * .map()
+ * .reduce()
+ *
+ * - Clone an array
+ * - Shuffle an array
+ */
+
+// names
+const names = ["Johan", "Kim", "Alicia", "Maja", "Elliot"];
+console.log("Names in chaos:", names);
