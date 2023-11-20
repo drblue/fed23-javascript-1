3886310ff742a7da2f97ac01ff4b034f461bfc74
Johan Nordström <johan@digitalvillage.se>
2023-11-20 13:56:47 +0100

    Filter all numbers >= 25 into a new array.

--- 28-higher-order-array-methods/filter.js
+++ 28-higher-order-array-methods/filter.js
@@ -16,11 +16,22 @@ const numbers = [ 42, 5, 21, 50, 24, 18, 47 ];
 
 console.log("Original numbers:", numbers);
 
+/*
 // push all numbers >= 25 into a new array
 const largeNumbers = [];
 numbers.forEach( (num) => {
 	if (num >= 25) {
 		largeNumbers.push(num);
+	}
+} );
+*/
+
+// filter out all numbers >= 25
+const largeNumbers = numbers.filter( (num) => {
+	if (num >= 25) {
+		return true;  // YES, this number should be in the array
+	} else {
+		return false;  // NOOOOOO, exclude this number from the new array
 	}
 } );
 
