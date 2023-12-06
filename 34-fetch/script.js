58a54fa4e033d5d0e9b032491fb46519db7b1480
Johan Nordström <johan@digitalvillage.se>
2023-12-06 11:33:33 +0100

    Get a JSON-file using Fetch.

--- 34-fetch/script.js
+++ 34-fetch/script.js
@@ -3,3 +3,24 @@
  *
  */
 
+console.log("starting...");
+
+fetch("data/cats.json")
+	.then(res => {
+		console.log("response:", res);
+
+		// Was request successful?
+		if (!res.ok) {
+			// Throw an error
+			throw new Error("Request was not successful!");
+		}
+
+		return res.json();
+	})
+	.then(data => {
+		console.log("data:", data);
+	})
+	.catch(err => {
+		console.log("Nope, something went wrong:", err);
+		// alert("Sorry, something went terribly wrong. Please try again never.");
+	});
