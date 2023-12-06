ba0dffe14a2dfe9b576b4dfee94fa119dfd08f82
Johan Nordström <johan@digitalvillage.se>
2023-12-06 15:06:26 +0100

    Chuck Norris Joke of the Day - Promise version.

--- 36-chuck-norris/script.js
+++ 36-chuck-norris/script.js
@@ -5,3 +5,17 @@
  * <https://api.chucknorris.io/jokes/random>
  */
 
+fetch("https://api.chucknorris.io/jokes/random")
+	.then(response => {
+		if (!response.ok) {
+			throw new Error("Chuck Norris is unavailable to take you call right now");
+		}
+
+		return response.json();
+	})
+	.then(joke => {
+		document.querySelector("#joke").innerText = joke.value;
+	})
+	.catch(err => {
+		document.querySelector("#joke").innerText = err;
+	});
