8f2594bff9e2aeb5ea858e90947a7652754d21db
Johan Nordström <johan@digitalvillage.se>
2023-12-06 13:32:19 +0100

    async/await boilerplate.

---
+++ 35-async-await/script.js
@@ -0,0 +1,21 @@
+/**
+ * async/await 🐶
+ *
+ */
+
+/*
+fetch("data/cats.json")
+	.then(res => {
+		if (!res.ok) {
+			throw new Error("Request was not successful!");
+		}
+
+		return res.json();
+	})
+	.then(data => {
+		console.log("data:", data);
+	})
+	.catch(err => {
+		console.log("Nope, something went wrong:", err);
+	});
+*/
