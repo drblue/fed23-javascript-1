9b4830160b1dec7d8147525965996d4cef7226ee
Johan Nordström <johan@digitalvillage.se>
2023-12-06 11:34:45 +0100

    Fetch `cats.json` and `dogs.json` in parallel.

--- 34-fetch/script.js
+++ 34-fetch/script.js
@@ -1,6 +1,7 @@
 /**
  * Fetch 🐶
  *
+ * Gets `cats.json` and `dogs.json` in parallel
  */
 
 console.log("starting...");
@@ -24,3 +25,25 @@ fetch("data/cats.json")
 		console.log("Nope, something went wrong:", err);
 		// alert("Sorry, something went terribly wrong. Please try again never.");
 	});
+
+fetch("data/dogs.json")
+	.then(res => {
+		console.log("response:", res);
+
+		// Was request successful?
+		if (!res.ok) {
+			// Throw an error
+			throw new Error("Request was not successful!");
+		}
+
+		return res.json();
+	})
+	.then(data => {
+		console.log("data:", data);
+	})
+	.catch(err => {
+		console.log("Nope, something went wrong:", err);
+		// alert("Sorry, something went terribly wrong. Please try again never.");
+	});
+
+console.log("done?!");
