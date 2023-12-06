f5fee64f520f979ddd960b202f587b648fe0e7bc
Johan Nordström <johan@digitalvillage.se>
2023-12-06 15:50:05 +0100

    Chuck Norris Joke of the Day - Catch errors in async/await using try/catch.

--- 36-chuck-norris/script.js
+++ 36-chuck-norris/script.js
@@ -6,13 +6,18 @@
  */
 
 const getJoke = async () => {
-	const response = await fetch("https://api.chucknorris.io/jokes/random");
-	if (!response.ok) {
-		throw new Error("Chuck Norris is unavailable to take you call right now");
-	}
+	try {
+		const response = await fetch("https://api.chucknorris.io/jokes/random");
+		if (!response.ok) {
+			throw new Error("Chuck Norris is unavailable to take you call right now");
+		}
 
-	const joke = await response.json();
-	document.querySelector("#joke").innerText = joke.value;
+		const joke = await response.json();
+		document.querySelector("#joke").innerText = joke.value;
+
+	} catch (err) {
+		document.querySelector("#joke").innerText = "ERROR: You don't summon Chuck Norris, Chuck Norris summons you";
+	}
 }
 
 // Listen for when Chuck Norris is summoned
