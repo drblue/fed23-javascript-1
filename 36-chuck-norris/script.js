360a4d2fd60f57b692f329a498b1b6f88930d432
Johan Nordström <johan@digitalvillage.se>
2023-12-06 15:25:50 +0100

    Chuck Norris Joke of the Day - But with button for moar lolz!

--- 36-chuck-norris/script.js
+++ 36-chuck-norris/script.js
@@ -5,17 +5,28 @@
  * <https://api.chucknorris.io/jokes/random>
  */
 
-fetch("https://api.chucknorris.io/jokes/random")
-	.then(response => {
-		if (!response.ok) {
-			throw new Error("Chuck Norris is unavailable to take you call right now");
-		}
+const getJoke = () => {
+	fetch("https://api.chucknorris.io/jokes/random")
+		.then(response => {
+			if (!response.ok) {
+				throw new Error("Chuck Norris is unavailable to take you call right now");
+			}
 
-		return response.json();
-	})
-	.then(joke => {
-		document.querySelector("#joke").innerText = joke.value;
-	})
-	.catch(err => {
-		document.querySelector("#joke").innerText = err;
-	});
+			return response.json();
+		})
+		.then(joke => {
+			document.querySelector("#joke").innerText = joke.value;
+		})
+		.catch(() => {
+			document.querySelector("#joke").innerText = "ERROR: You don't summon Chuck Norris, Chuck Norris summons you";
+		});
+}
+
+// Listen for when Chuck Norris is summoned
+document.querySelector("#getJokeBtn").addEventListener("click", () => {
+	// ?!
+	getJoke();
+});
+
+// Start of with a joke
+getJoke();
