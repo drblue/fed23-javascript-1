00767e1baeb688e22e589c6fc757f750db0fb1f1
Johan Nordström <johan@digitalvillage.se>
2023-12-11 15:26:27 +0100

    Extracted POST, PATCH and DELETE into separate functions.

--- 38-async-todos/frontend/script.js
+++ 38-async-todos/frontend/script.js
@@ -52,24 +52,8 @@ document.querySelectorAll("ul.todos").forEach(listEl => {
 				return;
 			}
 
-			// Change (toggle) completed on the found todo
-			clickedTodo.completed = !clickedTodo.completed;
-
-			// Send updated todo to the API
-			const patchResponse = await fetch("http://localhost:3001/todos/" + clickedTodoId, {
-				method: "PATCH",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify(clickedTodo),
-			});
-
-			// Was patch successful?
-			if (!patchResponse.ok) {
-				alert("Could not patch todo. Maybe someone deleted it? Try reloading the page or check the server");
-				console.log("Failed to patch todo. Response was:", patchResponse);
-				return;
-			}
+			// Ask API to update the todo (only send in the `completed` attribute)
+			await updateTodo(clickedTodoId, { completed: !clickedTodo.completed });
 
 			// Get the updated list of todos from the API
 			getTodos();
@@ -83,17 +67,8 @@ document.querySelectorAll("ul.todos").forEach(listEl => {
 			// Get the `data-todo-id` attribute from the parent
 			const clickedTodoId = Number(parentLiEl.dataset.todoId);
 
-			// Send DELETE-request to API for this todo
-			const deleteResponse = await fetch("http://localhost:3001/todos/" + clickedTodoId, {
-				method: "DELETE",
-			});
-
-			// Was delete successful?
-			if (!deleteResponse.ok) {
-				alert("Could not delete todo. Maybe someone else already deleted it? Try reloading the page or check the server");
-				console.log("Failed to delete todo. Response was:", deleteResponse);
-				return;
-			}
+			// Ask API to delete the todo
+			await deleteTodo(clickedTodoId);
 
 			// Get the updated list of todos from the API
 			getTodos();
@@ -123,24 +98,7 @@ formCreateTodoEl.addEventListener("submit", async (e) => {
 	console.log("Will send newTodo to API:", newTodo);
 
 	// POST todo to the API
-	const res = await fetch("http://localhost:3001/todos", {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify(newTodo),
-	});
-
-	// Check that everything went ok
-	if (!res.ok) {
-		alert("Could not create todo! 🥺");
-		console.log("Could not create todo:", res);
-		return;
-	}
-
-	// Get response-body
-	const data = await res.json();
-	console.log("Gots data back:", data);
+	await createTodo(newTodo);
 
 	// Get the updated list of todos from the API
 	getTodos();
@@ -191,6 +149,62 @@ const fetchTodos = async () => {
 	const data = await res.json();
 
 	return data;
+}
+
+// Create a new todo in the API
+const createTodo = async (newTodo) => {
+	const res = await fetch("http://localhost:3001/todos", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(newTodo),
+	});
+
+	// Check that everything went ok
+	if (!res.ok) {
+		alert("Could not create todo! 🥺");
+		console.log("Could not create todo:", res);
+		return;
+	}
+
+	// Get response-body
+	const data = await res.json();
+	console.log("Gots data back:", data);
+}
+
+// Delete a todo in the API
+const deleteTodo = async (id) => {
+	// Send DELETE-request to API for this todo
+	const deleteResponse = await fetch("http://localhost:3001/todos/" + id, {
+		method: "DELETE",
+	});
+
+	// Was delete successful?
+	if (!deleteResponse.ok) {
+		alert("Could not delete todo. Maybe someone else already deleted it? Try reloading the page or check the server");
+		console.log("Failed to delete todo. Response was:", deleteResponse);
+		return;
+	}
+}
+
+// Update a todo in the API
+const updateTodo = async (id, data) => {
+	// Send updated todo to the API
+	const patchResponse = await fetch("http://localhost:3001/todos/" + id, {
+		method: "PATCH",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(data),
+	});
+
+	// Was patch successful?
+	if (!patchResponse.ok) {
+		alert("Could not patch todo. Maybe someone deleted it? Try reloading the page or check the server");
+		console.log("Failed to patch todo. Response was:", patchResponse);
+		return;
+	}
 }
 
 // Get todos, replace existing todos and then render the todos
