6089675339f0311a07d677b177819e27ab9b7165
Johan Nordström <johan@digitalvillage.se>
2023-12-13 13:34:19 +0100

    Typing of function return values (or lack there of, as we usually want to infer the return type).

--- 40-typescript-vite/src/main.ts
+++ 40-typescript-vite/src/main.ts
@@ -23,6 +23,7 @@ let myName = "Johan";
 /**
  * Function parameter typing
  */
+/*
 const greet = (username: string, age: number) => {
 	console.log(`Hello, ${username}! You are ${age} years old.`);
 }
@@ -52,3 +53,24 @@ const greet3 = (username = "anonymouse", age?: number) => {
 greet3();
 greet3("hangry");
 greet3(undefined, 3);
+*/
+
+const fancyGreet = (username = "anonymouse", age?: number) => {
+	if (!age) {
+		return `Oh hello there good sir/ma'am, ${username}!`;
+	}
+	return `Oh hello there good sir/ma'am, ${username}! You look splendid for being ${age} years old.`;
+}
+
+const greeting = fancyGreet("Harold", 72);  // greeting will be inferred as a string, as fancyGreet only ever returns a string as the result
+console.log("fancyGreet returned:", greeting);
+
+const pets = ["cat", "dog", "hamster", "goldfish"];
+pets.forEach(pet => {
+	console.log(pet.toLocaleUpperCase());
+});
+
+const ages = [2, 4, 1, 3, 5, 2, 1, 3, 4, 5, 2, 1, 3, 4, 5];
+ages.forEach(age => {
+	// console.log(age.toLocaleUpperCase());  // Error: Property 'toLocaleUpperCase' does not exist on type 'number'.ts(2339)
+});
