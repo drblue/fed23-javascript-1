757a349630b78fa532858b76c79865bd88d6d20f
Johan Nordström <johan@digitalvillage.se>
2023-12-18 09:54:49 +0100

    Copied DOM-references and `renderTodos()` from `41-typescript-todos`.

--- 42-async-todos-ts/frontend/src/main.ts
+++ 42-async-todos-ts/frontend/src/main.ts
@@ -1,2 +1,22 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import './style.css';
+
+const todolistEl = document.querySelector<HTMLUListElement>("#todolist")!;
+const formCreateTodoEl = document.querySelector<HTMLFormElement>("#formCreateTodo");
+const inputNewTodoTitleEl = document.querySelector<HTMLInputElement>("#inputNewTodoTitle");
+
+let todos = [];
+
+/**
+ * Render Todos to DOM
+ */
+const renderTodos = () => {
+	todolistEl.innerHTML = todos
+		.map(todo => `
+			<li class="list-group-item ${todo.completed ? 'completed': ''}" data-todo-id="${todo.id}">
+				<span class="todo-title" role="button">${todo.title}</span>
+				<button class="ms-1 btn btn-danger btn-sm">🚮</button>
+			</li>
+		`)
+		.join("");
+}
