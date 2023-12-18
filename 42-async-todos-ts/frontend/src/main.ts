0e4b2abfb8db125d34f19eeae2b37de3efd81b54
Johan Nordström <johan@digitalvillage.se>
2023-12-18 11:49:01 +0100

    Create a new todo.

--- 42-async-todos-ts/frontend/src/main.ts
+++ 42-async-todos-ts/frontend/src/main.ts
@@ -11,8 +11,30 @@ interface Todo {
 	completed: boolean
 }
 
+interface NewTodo {
+	title: string
+	completed: boolean
+}
+
 let todos: Todo[] = [];
 //     ^?
+
+/**
+ * Create a new todo in the API
+ */
+const createTodo = async (newTodo: NewTodo) => {
+	const res = await fetch("http://localhost:3001/todos", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(newTodo),
+	});
+
+	if (!res.ok) {
+		throw new Error(`Could not create todo. Status code was: ${res.status}`);
+	}
+}
 
 /**
  * Fetch todos from API
@@ -53,6 +75,43 @@ const renderTodos = () => {
 		`)
 		.join("");
 }
+
+/**
+ * Listen for create todo form submit
+ */
+formCreateTodoEl?.addEventListener("submit", async (e) => {
+	// Stop form from being submitted to the server
+	// and causing a page reload
+	e.preventDefault();
+
+	// Get title from input-field (default to empty string if element does not exist)
+	const newTodoTitle = inputNewTodoTitleEl?.value || "";
+
+	// Abort if input is empty
+	if (newTodoTitle.length < 3) {
+		alert("That's not a todo");
+		return;
+	}
+
+	// Create a new todo-object
+	const newTodo: NewTodo = {
+		title: newTodoTitle,
+		completed: false,
+	}
+	console.log("Will send newTodo to API:", newTodo);
+
+	// POST todo to the API
+	await createTodo(newTodo);
+
+	// Get the updated list of todos from the API
+	getAndRenderTodos();
+
+	// Finally, clear the input-field
+	if (inputNewTodoTitleEl) {
+		inputNewTodoTitleEl.value = "";
+	}
+});
+
 
 // Get and render todos on page load
 getAndRenderTodos();
