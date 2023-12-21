4c4d5c89b31f06b4d63b03a23fb02e73a83c83af
Johan Nordström <johan@digitalvillage.se>
2023-12-21 10:37:41 +0100

    Copied JavaScript-code for DOM and API from `37-weather-app`.

--- 43-weather-app-ts/src/services/owmapi.ts
+++ 43-weather-app-ts/src/services/owmapi.ts
@@ -4,3 +4,27 @@
  * <https://openweathermap.org/current>
  */
 
+const API_KEY = "7d07164c415624a8f477cd253d231922";
+const BASE_URL = "https://api.openweathermap.org/data/2.5";
+
+const FAKE_SLOW_API = true;
+const FAKE_SLOW_API_DELAY = 2000;
+
+export const getCurrentWeather = async (city: string) => {
+	// Query API for current weather conditions in `city`
+	const response = await fetch(`${BASE_URL}/weather?q=${city}&units=metric&appid=${API_KEY}`);
+
+	// Check if we got an OK response
+	if (!response.ok) {
+		throw new Error(`${response.status} ${response.statusText}`); // 404 Not Found
+	}
+
+	// Convert response from JSON
+	const data = await response.json();
+
+	// Fake slow API
+	FAKE_SLOW_API && await new Promise(r => setTimeout(r, FAKE_SLOW_API_DELAY));
+
+	// Return current weather conditions
+	return data;
+}
